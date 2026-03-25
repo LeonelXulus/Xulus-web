bd061a5680ba756937b3b5411637c87dd26b00af
LeonelMFalcon <88933494+LeonelMFalcon@users.noreply.github.com>
2026-03-24 22:19:15 -0300

    update /dist

--- dist/xulus-web/server/main.server.mjs
+++ dist/xulus-web/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-JH4WW6RX.mjs";import"./chunk-DVTCP4UK.mjs";import"./chunk-ICTGMID2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-S2DSY57M.mjs";import"./chunk-DVTCP4UK.mjs";import"./chunk-ICTGMID2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
